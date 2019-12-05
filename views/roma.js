6f4f9d5226a2722077a12a2e3d0e8213788ad662
lucasThibord <50641511+lucasThibord@users.noreply.github.com>
2019-12-05 10:12:29 +0100

    update scripts jeu

--- views/roma.js
+++ views/roma.js
@@ -1,27 +1,43 @@
-var settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://wft-geo-db.p.rapidapi.com/v1/geo/cities/60577",
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "wft-geo-db.p.rapidapi.com",
-		"x-rapidapi-key": "f87d0a7432msh8eeb7a47bd9938cp1533f6jsnc0b92c8a3715"
-	}
-}
+var API_KEY = '14519071-008db298519f1ee2fa615f73b';
+var liste_pays=["Afghanistan", "Albania", "Algeria", "American Samoa", "Andorra", "Angola", "Anguilla", "Antarctica", "Antigua and Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia and Herzegowina", "Botswana", "Bouvet Island", "Brazil", "British Indian Ocean Territory", "Brunei Darussalam", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands", "Central African Republic", "Chad", "Chile", "China", "Christmas Island", "Cocos (Keeling) Islands", "Colombia", "Comoros", "Congo", "Congo, the Democratic Republic of the", "Cook Islands", "Costa Rica", "Cote d'Ivoire", "Croatia (Hrvatska)", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "East Timor", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Falkland Islands (Malvinas)", "Faroe Islands", "Fiji", "Finland", "France", "France Metropolitan", "French Guiana", "French Polynesia", "French Southern Territories", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guadeloupe", "Guam", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Heard and Mc Donald Islands", "Holy See (Vatican City State)", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran (Islamic Republic of)", "Iraq", "Ireland", "Israel", "Italy", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Korea, Democratic People's Republic of", "Korea, Republic of", "Kuwait", "Kyrgyzstan", "Lao, People's Democratic Republic", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libyan Arab Jamahiriya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Macedonia, The Former Yugoslav Republic of", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Martinique", "Mauritania", "Mauritius", "Mayotte", "Mexico", "Micronesia, Federated States of", "Moldova, Republic of", "Monaco", "Mongolia", "Montserrat", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauru", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Niue", "Norfolk Island", "Northern Mariana Islands", "Norway", "Oman", "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Pitcairn", "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", "Russian Federation", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Seychelles", "Sierra Leone", "Singapore", "Slovakia (Slovak Republic)", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Georgia and the South Sandwich Islands", "Spain", "Sri Lanka", "St. Helena", "St. Pierre and Miquelon", "Sudan", "Suriname", "Svalbard and Jan Mayen Islands", "Swaziland", "Sweden", "Switzerland", "Syrian Arab Republic", "Taiwan, Province of China", "Tajikistan", "Tanzania, United Republic of", "Thailand", "Togo", "Tokelau", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Turks and Caicos Islands", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States", "United States Minor Outlying Islands", "Uruguay", "Uzbekistan", "Vanuatu", "Venezuela", "Vietnam", "Virgin Islands (British)", "Virgin Islands (U.S.)", "Wallis and Futuna Islands", "Western Sahara", "Yemen", "Yugoslavia", "Zambia", "Zimbabwe"
+];
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+};
 
-$.ajax(settings).done(function (response) {
-	console.log(response);
+  var i = getRndInteger(0,120);
   
-  var country = response.data.country;
-  var population = response.data.population;
-  var region = response.data.region;
-  var longitude = response.data.longitude;
-  var latitude = response.data.latitude;
-  
+  /*var URL = "https://pixabay.com/api/?key="+API_KEY+"&q="+liste_pays[i]+"&image_type=photo&category=places&safesearch=true&per_page=3"
+$.getJSON(URL, function(data){
+if (parseInt(data.totalHits) > 0)
+    $.each(data.hits, function(i, hit){ console.log(hit.pageURL); });
+else
+    console.log('No hits');
+});*/
+var URL = "https://pixabay.com/api/?key=YOUR_API_KEY&q="+liste_pays[i];
+$.getJSON(URL, function(data) {
+	var largeImageURL = data[0].largeImageURL;
+$("largeImageURL").append(largeImageURL);
+});
+  //var pays =  <?php echo $pays;?>;
+$.getJSON("https://restcountries.eu/rest/v2/name/"+liste_pays[i], function(data) {
+//$.getJSON("https://restcountries.eu/rest/v2/name/"+pays, function(data) {
+  console.log(data);
+
+  var name = data[0].name
+  var capital = data[0].capital;
+  var superficy = data[0].area+" km²";
+  var region = data[0].subregion;
+  var currency = data[0].currencies[0].name;
+  var population = data[0].population;
+  var language = data[0].languages[0].name;
+
+   $(".name").append(name);
   $(".region").append(region);
-  $(".latitude").append(latitude);
-  $(".longitude").append(longitude);
+  $(".superficy").append(superficy);
+  $(".currency").append(currency);
+  $(".language").append(language);
+
+  $(".capital").append(capital);
   $(".population").append(population);
-  $(".country").append(country);
-  
 });
